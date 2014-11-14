6cc420020c08cfa1991053c511663251defcea4a
Rick Guyer <ricoguyer@gmail.com>
2014-11-13 22:30:35 -0600

    Remove git config

--- init.sh
+++ init.sh
@@ -3,13 +3,10 @@
 echo ""
 echo "-> Welcome Rick! Let's get started shall we."
 
-echo "-> Setting git credentials"
-git config --global user.name "Rick Guyer"
-git config --global user.email "ricoguyer@gmail.com"
-
 echo "-> Installing Kitchenplan to do the heavy lifting"
 #TODO - make sure gem is available
 gem install kitchenplan
+
 #TODO - make sure kitchenplan is installed and executable
 kitchenplan setup https://github.com/ricog/kitchenplan-config.git
 
