8facd0eda00e6f1f5b501973f34e0afffe11577e
Rick Guyer <ricoguyer@gmail.com>
2014-11-13 22:27:19 -0600

    Add Kitchenplan install and provision

--- init.sh
+++ init.sh
@@ -1,3 +1,17 @@
 #!/usr/local/bin/env bash
 
-echo 'Do something cool'
+echo ""
+echo "-> Welcome Rick! Let's get started shall we."
+
+echo "-> Setting git credentials"
+git config --global user.name "Rick Guyer"
+git config --global user.email "ricoguyer@gmail.com"
+
+echo "-> Installing Kitchenplan to do the heavy lifting"
+#TODO - make sure gem is available
+gem install kitchenplan
+#TODO - make sure kitchenplan is installed and executable
+kitchenplan setup https://github.com/ricog/kitchenplan-config.git
+
+echo "-> Running Kitchenplan"
+kitchenplan provision
